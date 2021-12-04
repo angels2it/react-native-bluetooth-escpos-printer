97693ca3789385e8f01d17ff7eb3c5a1791e6ce0
Kalvin Pratama <kalvin.pratama@flash-coffee.com>
2021-12-04 07:25:23 +0700

    latest label fitment

--- android/src/main/java/cn/jystudio/bluetooth/zpl/RNBluetoothZplPrinterModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/zpl/RNBluetoothZplPrinterModule.java
@@ -35,7 +35,7 @@ implements BluetoothServiceStateObserver{
         ZplCommand zpl = new ZplCommand();
         ReadableArray texts = options.hasKey("text")? options.getArray("text"):null;
         zpl.addStartCommand();
-        zpl.addLabelPositioning("250","240","30");
+        zpl.addLabelPositioning("252","220","20");
         for (int i = 0;texts!=null&& i < texts.size(); i++) {
             ReadableMap text = texts.getMap(i);
             String t = text.getString("text");
