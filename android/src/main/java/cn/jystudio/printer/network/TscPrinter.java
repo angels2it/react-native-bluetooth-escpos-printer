d2e1f3b125efa8293c752c72152505fc79c56667
Kalvin Pratama <kalvin.pratama@flash-coffee.com>
2022-02-16 12:54:22 +0700

    adjust printer performance for TSC TDP 225

--- android/src/main/java/cn/jystudio/printer/network/TscPrinter.java
+++ android/src/main/java/cn/jystudio/printer/network/TscPrinter.java
@@ -29,43 +29,48 @@ public class TscPrinter {
 
     public boolean printLabel() {
         TscCommand tsc = new TscCommand();
-        ReadableArray texts = options.hasKey("text")? options.getArray("text"):null;
-        String lbSize = options.hasKey("size")? options.getString("size"):"SIZE 40mm,30mm";
-        String lbGap = options.hasKey("gap")? options.getString("gap"):"GAP 3mm";
-        String lbDirection = options.hasKey("direction")? options.getString("direction"):"DIRECTION 1";
-        String fontEncoding = options.hasKey("fontEncoding")? options.getString("fontEncoding"):"GB2312";
-        String delay = options.hasKey("delay")? options.getString("delay"):"N";
-        String totalRep = options.hasKey("totalRep")? options.getString("totalRep") : "1";
-        String remainder = options.hasKey("remainder")? options.getString("remainder") : "1";
-        
-        tsc.addStartCommand(lbSize,lbGap,lbDirection,fontEncoding,remainder);
-        for (int i = 0;texts!=null&& i < texts.size(); i++) {
-            ReadableMap text = texts.getMap(i);
-            String t = text.getString("text");
-            String fontType = text.getString("fontType");
-            String x = text.getString("x");
-            String y = text.getString("y");
-            String fieldBlock = text.hasKey("fieldBlock") ? text.getString("fieldBlock") : "";
-            String fontMultiplier = text.hasKey("fontMultiplier") ? text.getString("fontMultiplier"): "1,1";
-            String useCounter = text.hasKey("useCounter") ? text.getString("useCounter") : "N";
-            try {
-                byte[] temp = t.getBytes(fontEncoding);
-                String temStr = new String(temp, fontEncoding);
-                t = new String(temStr.getBytes(fontEncoding), fontEncoding);//打印的文字
-            } catch (Exception e) {
-                //promise.reject("INVALID_TEXT", e);
-                //mmSocket.close();
-                return false;
-            }
-
-            if (fieldBlock!=""){
-                tsc.addFieldBlock(fieldBlock,x,y,fontType,fontMultiplier,t,fontEncoding);
-            } else {
-                tsc.addText(x,y,fontType,fontMultiplier,t,fontEncoding,useCounter);
-            }
+        ReadableArray commands = options.hasKey("commands")? options.getArray("commands"):null;
+        for (int idxCommand = 0;commands!=null&& idxCommand < commands.size(); idxCommand++) {
+            ReadableMap command = commands.getMap(idxCommand);
+            ReadableArray texts = command.hasKey("text")? command.getArray("text"):null;
+           String lbSize = command.hasKey("size")? command.getString("size"):"SIZE 40mm,30mm";
+            String lbGap = command.hasKey("gap")? command.getString("gap"):"GAP 3mm";
+            String lbDirection = command.hasKey("direction")? command.getString("direction"):"DIRECTION 1";
+            String fontEncoding = command.hasKey("fontEncoding")? command.getString("fontEncoding"):"GB2312";
+            String delay = command.hasKey("delay")? command.getString("delay"):"N";
+            String totalRep = command.hasKey("totalRep")? command.getString("totalRep") : "1";
+            String remainder = command.hasKey("remainder")? command.getString("remainder") : "1";
             
+            tsc.addStartCommand(lbSize,lbGap,lbDirection,fontEncoding,remainder);
+            for (int i = 0;texts!=null&& i < texts.size(); i++) {
+                ReadableMap text = texts.getMap(i);
+                String t = text.getString("text");
+                String fontType = text.getString("fontType");
+                String x = text.getString("x");
+                String y = text.getString("y");
+                String fieldBlock = text.hasKey("fieldBlock") ? text.getString("fieldBlock") : "";
+                String fontMultiplier = text.hasKey("fontMultiplier") ? text.getString("fontMultiplier"): "1,1";
+                String useCounter = text.hasKey("useCounter") ? text.getString("useCounter") : "N";
+                try {
+                    byte[] temp = t.getBytes(fontEncoding);
+                    String temStr = new String(temp, fontEncoding);
+                    t = new String(temStr.getBytes(fontEncoding), fontEncoding);//打印的文字
+                } catch (Exception e) {
+                    //promise.reject("INVALID_TEXT", e);
+                    //mmSocket.close();
+                    return false;
+                }
+
+                if (fieldBlock!=""){
+                    tsc.addFieldBlock(fieldBlock,x,y,fontType,fontMultiplier,t,fontEncoding);
+                } else {
+                    tsc.addText(x,y,fontType,fontMultiplier,t,fontEncoding,useCounter);
+                }
+                
+            }
+            tsc.addEndCommand(fontEncoding, totalRep);
         }
-        tsc.addEndCommand(fontEncoding, totalRep);
+        
         Vector<Byte> bytes = tsc.getCommand();
         byte[] tosend = new byte[bytes.size()];
         for(int i=0;i<bytes.size();i++){
